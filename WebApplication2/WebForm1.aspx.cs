bc22fe54512a33ddf71d405270d06fe97e29b232
Naveed Shah <naveed@tayybah.com>
2018-07-03 16:07:03 -0400

    updated to call 1A and accept new input file format

--- WebApplication2/WebForm1.aspx.cs
+++ WebApplication2/WebForm1.aspx.cs
@@ -33,12 +33,11 @@ namespace WebApplication2
                     LineDictionary = new Dictionary<string, string>();
                     LineDictionary["borough"] = line.Substring(0, 1);
                     LineDictionary["hn1"] = line.Substring(1, 11);
-                    LineDictionary["hn2"] = line.Substring(14, 11);
+                    LineDictionary["hn2"] = line.Substring(13, 11);
                     LineDictionary["street"] = line.Substring(25, 31);
-                    LineDictionary["organic"] = line.Substring(57, 4);
-                    LineDictionary["district"] = line.Substring(62, 3);
-                    LineDictionary["subsection"] = line.Substring(65, 2);
-                    LineDictionary["filler"] = filler.PadRight(207, ' ');
+                    LineDictionary["district"] = line.Substring(57, 2);
+                    LineDictionary["subsection"] = line.Substring(60, 2);
+                    LineDictionary["filler"] = filler.PadRight(212, ' ');
                     PageDictionary[counter] = LineDictionary;
                     counter++;
                 }
@@ -46,7 +45,8 @@ namespace WebApplication2
                 counter = 1;
                 Wa1 mywa1;
                 geo mygeo;
-                Wa2F5 mywa2f5;
+                //Wa2F5 mywa2f5;
+                Wa2F1ax mywa2f1ax;
 
                 string ElevenSpaces = "           ";
 
@@ -55,24 +55,25 @@ namespace WebApplication2
 
                     mywa1 = new Wa1();
                     mygeo = new geo();
-                    mywa2f5 = new Wa2F5();
+                    mywa2f1ax = new Wa2F1ax();
 
                     mywa1.in_boro1 = PageDictionary[counter]["borough"];
                     mywa1.in_hnd  = PageDictionary[counter]["hn1"];
                     mywa1.in_low_hnd = PageDictionary[counter]["hn2"];
                     mywa1.in_stname1 = PageDictionary[counter]["street"];
 
-                    mywa1.in_func_code = "5";
+                    mywa1.in_func_code = "1A";
                     mywa1.in_platform_ind = "C";
+                    mywa1.in_mode_switch = "X";
 
-                    mygeo.GeoCall(ref mywa1, ref mywa2f5);
+                    mygeo.GeoCall(ref mywa1, ref mywa2f1ax);
 
                     string strPath = Environment.GetFolderPath(
                          System.Environment.SpecialFolder.DesktopDirectory);
 
                     if (mywa1.out_grc == "00" || mywa1.out_grc == "01") {
 
-                        string newLine = mywa2f5.gridkey1.ToString() + ElevenSpaces + PageDictionary[counter]["district"] + PageDictionary[counter]["subsection"] + PageDictionary[counter]["organic"] + PageDictionary[counter]["filler"];
+                        string newLine = mywa2f1ax.gridkey1.ToString() + ElevenSpaces + PageDictionary[counter]["district"] + PageDictionary[counter]["subsection"] + PageDictionary[counter]["filler"];
 
                         string text = Environment.NewLine + newLine;
 
@@ -80,8 +81,8 @@ namespace WebApplication2
 
                         counter++;
                     } else {
-                        string text = Environment.NewLine + mywa1.out_error_message;
-                        System.IO.File.AppendAllText(strPath + "\\results.txt", text);
+                        string text = Environment.NewLine + PageDictionary[counter]["borough"] + " " + PageDictionary[counter]["hn1"]  + PageDictionary[counter]["street"] + mywa1.out_error_message;
+                        System.IO.File.AppendAllText(strPath + "\\errors.txt", text);
                     }
                      
                 }
